fa9f9389cb00f83241f78f8eb70bda73dbbc8878
admin <admin@CRN-S20-iMac-ITK-16.local>
2023-01-13 14:07:20 +0100

    Add default and home front base

--- app/views/default.php
+++ app/views/default.php
@@ -2,27 +2,44 @@
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mediathèque</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mediathèque</title>
 
-    <link rel="stylesheet" href="/assets/css/main.min.css">
+  <link rel="stylesheet" href="/assets/css/main.min.css">
 </head>
 
 <body>
-    <header>
-        <nav>
-          
-        </nav>
-    </header>
+  <header>
+    <div>
+      <a href="">Home</a>
+    </div>
+    <div>
+      <img src="https://picsum.photos/35" alt="" />
+      <nav>
+        <?php
+        if ($datas[0] === false) { ?>
+          <ul>
+            <li><a href="">Connexion</a></li>
+            <li><a href="">Inscription</a></li>
+          </ul>
+        <?php } else { ?>
+          <ul>
+            <li><a href="">Profil</a></li>
+            <li><a href="">Déconnexion</a></li>
+          </ul>
+        <?php } ?>
+      </nav>
+    </div>
+  </header>
 
-    <main class="main-container">
-        <?= $contenu ?>
-    </main>
+  <main class="main-container">
+    <?= $contenu ?>
+  </main>
 
-    <footer>
-        <p>&copy; IT-Students all rights reserved</p>
-    </footer>
+  <footer>
+    <p>&copy; IT-Students all rights reserved</p>
+  </footer>
 
 </body>
 
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,37 @@
-Hello World !
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mediathèque</title>
+
+  <link rel="stylesheet" href="/assets/css/main.min.css">
+</head>
+
+<body>
+
+  <main class="main-container">
+    <form class="SearchBar">
+      <input type="text">
+      <button><img src="../../../assets/images/loupe.png" alt="Rechercher" width="15px"></button>
+    </form>
+    <!-- Affichage des images choppées en API -->
+    <div class="caroussel">
+      <img src="http://placekitten.com/400/400" />
+      <img src="http://placekitten.com/400/400" />
+      <form class="activePicture" action="">
+        <img src="http://placekitten.com/400/400" />
+
+        <button><img src="../../../assets/images/sauvegarder.png" width="15px"></button>
+      </form>
+      <img src="http://placekitten.com/400/400" />
+
+      <img src="http://placekitten.com/400/400" />
+
+    </div>
+  </main>
+
+</body>
+
+</html>
